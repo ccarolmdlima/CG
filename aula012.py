53c30e4d0a4bdf5f6d92fbed70233da6451c1efd
carol <126472626+ccarolmdlima@users.noreply.github.com>
2024-02-26 10:45:52 -0300

    Create aula012.py

---
+++ aula012.py
@@ -0,0 +1,15 @@
+import turtle as tr
+
+tr.shape("turtle")
+tr.pensize(5)
+tr.speed(50)
+
+tr.circle(100)
+tr.penup()
+tr.backward(250)
+tr.pendown()
+tr.circle(100)
+tr.penup()
+tr.forward(500)
+tr.pendown()
+tr.circle(100)
