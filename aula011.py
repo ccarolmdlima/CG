016766c8ccb3e3b6e5fb890f582ca9616553ff0d
carol <126472626+ccarolmdlima@users.noreply.github.com>
2024-02-26 10:39:08 -0300

    Create aula011.py

---
+++ aula011.py
@@ -0,0 +1,13 @@
+import turtle as tr
+
+tr.shape("turtle")
+tr.pensize(5)
+tr.color("pink")
+tr.bgcolor("purple")
+tr.speed(100)
+
+tr.circle(100)
+tr.penup()
+tr.forward(100)
+tr.pendown()
+tr.circle(100)
