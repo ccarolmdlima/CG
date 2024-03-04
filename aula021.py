02ab513eea73436f439c8617be9a58bc7d643db7
Carolina <126472626+ccarolmdlima@users.noreply.github.com>
2024-03-04 16:52:45 -0300

    Update aula021.py

--- aula021.py
+++ aula021.py
@@ -1,6 +1,10 @@
 import turtle as tr
 
-tr.color(176/255,147/255,130/255)
+tr.speed(7)
+tr.bgcolor(149/255,189/255,252/255)
+
+#casa
+tr.color(94/255,75/255,59/255)
 tr.pensize(5)
 tr.begin_fill()
 
@@ -20,6 +24,7 @@ tr.forward(150)
 tr.right(90)
 tr.pendown()
 
+#telhado
 tr.color(153/255,14/255,14/255)
 tr.begin_fill()
 
@@ -40,6 +45,7 @@ tr.left(90)
 tr.forward(55)
 tr.pendown()
 
+#porta
 tr.color(94/255,75/255,59/255)
 tr.begin_fill()
 
@@ -58,3 +64,76 @@ tr.forward(55)
 tr.right(90)
 tr.forward(75)
 tr.right(90)
+tr.forward(10)
+tr.pendown()
+
+#janela
+tr.color(94/255,75/255,59/255)
+tr.begin_fill()
+
+sides = 4
+angle = 360 / 4
+
+for x in range(sides):
+    tr.forward(40)
+    tr.left(angle)
+
+tr.color(149/255,189/255,252/255)
+tr.end_fill()
+
+tr.penup()
+tr.forward(20)
+tr.left(90)
+
+#linhas da janela
+tr.color(94/255,75/255,59/255)
+tr.pendown()
+tr.forward(40)
+
+tr.penup()
+tr.left(90)
+tr.forward(20)
+tr.left(90)
+tr.forward(20)
+tr.left(90)
+tr.pendown()
+tr.forward(40)
+
+tr.penup()
+tr.right(90)
+tr.forward(20)
+tr.left(90)
+tr.forward(50)
+tr.pendown()
+
+#janela
+tr.color(94/255,75/255,59/255)
+tr.begin_fill()
+
+sides = 4
+angle = 360 / 4
+
+for x in range(sides):
+    tr.forward(40)
+    tr.left(angle)
+
+tr.color(149/255,189/255,252/255)
+tr.end_fill()
+
+tr.penup()
+tr.forward(20)
+tr.left(90)
+
+#linhas da janela
+tr.color(94/255,75/255,59/255)
+tr.pendown()
+tr.forward(40)
+
+tr.penup()
+tr.left(90)
+tr.forward(20)
+tr.left(90)
+tr.forward(20)
+tr.left(90)
+tr.pendown()
+tr.forward(40)
