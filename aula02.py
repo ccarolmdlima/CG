40fc8c78d881e319e088c7f17c3b7900678622d3
Carolina <126472626+ccarolmdlima@users.noreply.github.com>
2024-03-04 09:30:10 -0300

    Update aula02.py

--- aula02.py
+++ aula02.py
@@ -1,6 +1,7 @@
 import turtle as tr
 
 tr.color(145/255,2/255,247/255)
+tr.pensize(5)
 tr.begin_fill()
 
 tr.forward(100)
@@ -8,5 +9,6 @@ tr.left(120)
 tr.forward(100)
 tr.left(120)
 tr.forward(100)
+tr.color(3/255,255/255,61/255)
 
 tr.end_fill()
